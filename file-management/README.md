7701f531d9c35eac53737e17c60d723497f73a0c
kit <info@rair.dev>
2021-05-28 11:22:04 +0200

    Update smpdf to accept filenames in command. Update README

--- file-management/README.md
+++ file-management/README.md
@@ -1,8 +1,23 @@
 # Basic File management
 
-*smpdf*
+## smpdf
 
-Simple tool that finds all PDF file within the selected folder, downsizes them with reasonable ghostscript settings.
-- Replaces spaces in names with underscores for naming simplicity
-  - Have not implemented any other character replacements that might break the script
-- Name the new compressed PDF with _small at the end of the file name.
+Simple tool that accepts multiple PDF files, downsizes them with reasonable ghostscript settings.
+
+*Install*
+
+Place the script in a location where it can be found with $PATH. I would recommend something simple like: ~/.local/bin
+
+Ensure you have the ability to run it by ````chmod 755 smpdf.sh````
+
+*Usage*
+
+````smpdf <filename1.pdf> <file2.pdf> ... <fileN.pdf>````
+
+*What it Does*
+- Rename the new compressed PDF with _small at the end of the file name. Place in a subfolder called small_pdf
+
+*Limitations*
+- No checking to whether or not the file is actually a PDF to begin with
+- Cannot guarantee that output file will be smaller than input depending on what it is
+- Certain characters in the file name might cause the script to break
--- file-management/smpdf.sh
+++ file-management/smpdf.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
-for file in `find *.pdf`
+# Small script to optimize pdf's quickly
+# Depends on ghostscript
+# Written by Kit Rairigh - https://github.com/krair - https://rair.dev
 
+set -e
+
+# Check for number of files given, add to variable
+if (( $# > 0 )); then
+## TODO - Add a check to make sure file is a PDF, file(s) exist, etc.
+files=("$@")
+else
+echo "Please provide at least 1 valid filename"
+fi
+
+# Create a subdirectory titled small_pdf to store the downsized pdfs
+if [[ ! -d "./small_pdf" ]]; then
+mkdir ./small_pdf
+fi
+
+# Downsize each file, save in above directory, add _small to the end of the filename
+for ((i = 0; i < ${#files[@]}; i++))
 do
+	# If a name is not a valid file, continue
+	if [[ ! -f "${files[$i]}" ]];then continue
+	fi
+	# get filename
+	fname=$(basename "${files[$i]}" .pdf)
+  # use ghostscript with reasonable balance between size and quality
+	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=./small_pdf/"${fname}"_small.pdf "${files[$i]}"
 
-	fname=`basename $file | tr ' ' '_'`
-
-	gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${fname}_small.pdf $file
-	
-	echo "${fname} resized"
+	echo "${files[$i]} resized"
 
 done
