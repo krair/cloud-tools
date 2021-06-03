c290bebc260c9bbd5d8507d47a3bd22247e0d802
kit <info@rair.dev>
2021-06-03 11:36:00 +0200

    Add function to check for Admin priviledge, rerun script elevated

--- wireguard/Win_wg_adapters_to_private.ps1
+++ wireguard/Win_wg_adapters_to_private.ps1
@@ -1,10 +1,35 @@
+#############################################################################
+# Windows Powershell Script to change Wireguard interfaces from the Public to
+# Private network category.
+#
+# Written by: Kit Rairigh - https://github.com/krair - https://rair.dev
+#############################################################################
+
+# The first part looks for Administrator privileges, as this change can only
+# be accomplished as Administrator
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
 # Get a list of the Wireguard interface names
 $wgcfg = Split-Path 'C:\Program Files\Wireguard\Data\Configurations\*' -leaf -Resolve | ForEach-Object {$_.Split('.')[0]}
 
 # Set any active Public Wireguard Adapters to Private
 Try {
     # List all active Internet adapters set to 'Public'
-    Get-NetConnectionProfile -NetworkCategory 'Public' -ErrorAction stop | 
+    Get-NetConnectionProfile -NetworkCategory 'Public' -ErrorAction stop |
     ForEach-Object {
         # Check if Public adapter is also a Wireguard interface
         if ( $_.InterfaceAlias -in $wgcfg ) {
